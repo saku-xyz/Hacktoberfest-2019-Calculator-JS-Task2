e68eda0c2505d6efc7414593942e7b32eae8e647
ppubudu4 <ppubudu4@gmail.com>
2019-10-23 23:48:51 +0530

    complete the tasks

--- task.js
+++ task.js
@@ -10,6 +10,10 @@ const performCalculation = {
     '+': (firstOperand, secondOperand) => firstOperand + secondOperand,
 
     '-': (firstOperand, secondOperand) => firstOperand - secondOperand,
+	
+	'*': (firstOperand, secondOperand) => firstOperand * secondOperand,
+	
+	'/': (firstOperand, secondOperand) => firstOperand / secondOperand,
 
     '=': (firstOperand, secondOperand) => secondOperand
 };
